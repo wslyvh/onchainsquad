3fbe40e2d771f104e1f695daed77c9acf0581b28
wslyvh <wslyvh@users.noreply.github.com>
2023-12-22 14:00:15 +0100

    form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           unoptimized
         />
 
-        <form action="https://login.sendpulse.com/forms/simple/u/eyJ1c2VyX2lkIjo4Mzk2NTA3LCJhZGRyZXNzX2Jvb2tfaWQiOjQ4MzgwNywibGFuZyI6ImVuIn0=" method="post" target='_blank'>
+        <form action="https://login.sendpulse.com/forms/simple/u/eyJ1c2VyX2lkIjo4Mzk2NTA3LCJhZGRyZXNzX2Jvb2tfaWQiOjQ4MzgwNywibGFuZyI6ImVuIn0=" target="_blank" method="post">
           <div className='flex flex-col text-neutral-content items-center text-center gap-8'>
             <h1 className='text-2xl font-bold'>
               Soon™️
@@ -23,7 +23,7 @@ export default function Home() {
 
             <div className='flex flex-col gap-4'>
               <input className='input input-bordered input-sm w-72' name="email" type="email" placeholder='Enter your email..' required />
-              <input type="hidden" name="sender" value="wesley@useweb3.xyz" />
+              <input type="hidden" name="sender" value="hello@onchainsquad.com" />
               <input type="hidden" name="name" value="" />
               <button className='btn btn-primary btn-sm w-72'>Subscribe</button>
             </div>
