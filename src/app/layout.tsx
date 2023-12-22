f5be576312dba0b3ebcb1811d0834ea3d3e520f4
wslyvh <wslyvh@users.noreply.github.com>
2023-12-22 14:28:09 +0100

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,25 @@ import type { Metadata } from 'next'
 import { PropsWithChildren } from 'react'
 import './assets/globals.css'
 
+const title = 'Open Source | Onchain Squad'
+const description = 'Join us for 30-days of Web3 and make contribute to open-source software and crypto projects'
+
 export const metadata: Metadata = {
-  title: 'Open Source | Onchain Squad',
-  description: 'Join us for 30-days of Web3 and contribute to onchain open-source software',
+  title: title,
+  description: description,
+  openGraph: {
+    type: 'website',
+    title: title,
+    description: description,
+    url: 'https://www.onchainsquad.com',
+    images: 'https://www.onchainsquad.com/og.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: title,
+    description: description,
+    images: 'https://www.onchainsquad.com/og.png',
+  },
 }
 
 export default function RootLayout(props: PropsWithChildren) {
